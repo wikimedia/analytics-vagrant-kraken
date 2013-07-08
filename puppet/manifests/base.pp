e76ca2c0e2b827032c06e5896dd8dc94c8317143
Andrew Otto <acotto@gmail.com>
2013-07-08 11:06:23 -0400

    Removing unused stuff from base.pp

--- puppet/manifests/base.pp
+++ puppet/manifests/base.pp
@@ -1,16 +1,12 @@
 class base {
   class { 'base::java':           }
   class { 'base::apt::wikimedia': }
-  # class { 'base::apt::cdh4':      }
 }
 
 class base::java {
-  java { 'openjdk': distribution => 'openjdk' }
+  java { 'java': distribution => 'oracle', version => '6' }
 }
 
-# class base::local_hostname {
-#     
-# }
 
 class base::apt::wikimedia {
   class { 'apt::update': }
@@ -37,31 +33,3 @@ class base::apt::wikimedia {
     require => Apt::Repository['wikimedia'],
   }
 }
-
-# TODO: make this use apt module?
-class base::apt::cdh4 {
-  $operatingsystem_lowercase = inline_template('<%= operatingsystem.downcase %>')
-  $cdhversion = 4
-
-  file { '/etc/apt/sources.list.d/cdh4.list':
-    content => "deb [arch=${architecture}] http://archive.cloudera.com/cdh4/${operatingsystem_lowercase}/${lsbdistcodename}/${architecture}/cdh ${lsbdistcodename}-cdh${cdhversion} contrib\ndeb-src http://archive.cloudera.com/cdh4/${operatingsystem_lowercase}/${lsbdistcodename}/${architecture}/cdh ${lsbdistcodename}-cdh${cdhversion} contrib\n",
-    mode    => 0444,
-    ensure  => 'present',
-  }
-
-  exec { "import_cloudera_apt_key":
-    command   => "/usr/bin/curl -s http://archive.cloudera.com/cdh4/${operatingsystem_lowercase}/${lsbdistcodename}/${architecture}/cdh/archive.key | /usr/bin/apt-key add -",
-    subscribe => File['/etc/apt/sources.list.d/cdh4.list'],
-    unless    => '/usr/bin/apt-key list | /bin/grep -q Cloudera',
-  }
-
-  exec { 'apt_get_update_for_cloudera':
-    command     => '/usr/bin/apt-get update',
-    timeout     => 240,
-    returns     => [ 0, 100 ],
-    refreshonly => true,
-    subscribe   => [File['/etc/apt/sources.list.d/cdh4.list'], Exec['import_cloudera_apt_key']],
-  }
-}
-
-
